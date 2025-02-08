9c280843a0bd59cee2e0a53e29742b642a6fd718
ahmedammad1709 <ttorfidoo001@gmail.com>
2025-02-08 12:45:54 +0500

    Added Javascript to enhance functionality of Progress bar

--- index.js
+++ index.js
@@ -19,22 +19,37 @@ setInterval(() => {
 function showAnalog() {
     document.getElementById("clockcontainer").style.display = "block";
     document.getElementById("digitalClock").style.display = "none";
+   restartProgressBar();
 }
 
 function showDigital(){
     document.getElementById("clockcontainer").style.display = "none";
     document.getElementById("digitalClock").style.display = "block";
+    restartProgressBar();
+}
+
+//For Progress bar
+function restartProgressBar() {
+    let progressBar = document.querySelector(".progressbar");
+
+    progressBar.style.width = "0"; 
+    progressBar.style.animation = "none"; 
+    progressBar.offsetWidth; // Trigger reflow
+
+    // Reapply animation after a short delay
+    setTimeout(() => {
+        progressBar.style.animation = "load 0.5s ease-in-out forwards";
+    }, 10);
 }
 
 //For Digital
-
 setInterval(() => {
     const now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
 
-    // Add leading zeros if needed
+    // Adding leading zeros 
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
